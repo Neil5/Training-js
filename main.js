3b1fe5006cfa96c4697c7ac96e70bcb60b205996
Bhargav Anand <rjn143@gmail.com>
2015-12-01 19:56:37 -0800

    Task: added comments to main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-// Hoisting
+/**
+ * Hoisting
+ * Add about hoisting
+ */
 var a;
 a = 10;
 
@@ -7,8 +10,10 @@ var b = 5;
 c = 10;
 console.log(a, b, c);
 
-// Scoping
-
+/**
+ * Scoping
+ * Explain about scoping
+ */
 function hello() {
   var d = 20;
   console.log(a);
@@ -23,3 +28,13 @@ if (a > 10) {
 hello();
 // console.log(d);
 console.log(e);
+
+
+/**
+* Ways to select DOM elements.
+* 1. document.getElementById(x):  Selects a DOM element with an id of x
+* 2. document.querySelector(y): Selects a DOM element with a class of y
+* 3. document.querySelectorAll(z): Selects a DOM element(s) with a class of z
+**/
+
+var content = document.getElementById('content');
