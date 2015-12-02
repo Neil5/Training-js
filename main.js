2f5f70423d8a5012650e69cb4f70153cbaf0f95f
Bhargav Anand <rjn143@gmail.com>
2015-12-01 19:48:25 -0800

    Task: add scoping to main.js

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ c = 10;
 console.log(a, b, c);
 
 // Scoping
+
+function hello() {
+  var d = 20;
+  console.log(a);
+}
+
+if (a > 10) {
+  var e = 20;
+} else {
+  var e = 90
+}
+
+hello();
+// console.log(d);
+console.log(e);
